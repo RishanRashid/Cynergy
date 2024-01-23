19ff68515b84420ccecd3e0b0863dbb1d72cd1a5
RishanRashid <rishanrashi@gmail.com>
2024-01-23 22:09:39 +0530

    Design Glitch

--- cynergyTask/Model/EscapeModel.swift
+++ cynergyTask/Model/EscapeModel.swift
@@ -12,3 +12,22 @@ struct EscapeModel: Codable {
     let response: String?
     let token: String?
 }
+struct SplashImages {
+    let splashImageTop, splashImageBottom: String
+}
+struct LoginModel {
+    let response: String
+    let user: User
+    let foodMenu, attractionsMenu: Int
+    let splashImages: SplashImages
+}
+struct User {
+    let id, roleID, locationID: Int
+    let address1, address2, city, zip: NSNull
+    let bookingToken, accessToken: NSNull
+    let name: String
+    let firstName, lastName, email: NSNull
+    let phoneCode, phone: String
+    let image, gender, birthday, cinemaID: NSNull
+    let memberID: NSNull
+}
